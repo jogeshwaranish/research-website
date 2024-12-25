211be119eae707fe940d75d9d6fae2bb03100aa0
Anish <ani.bellamkonda@gmail.com>
2024-12-24 19:54:16 -0600

    added more routes

--- app/app.py
+++ app/app.py
@@ -23,30 +23,44 @@ class User(db.Model):
 def main_page():
     return render_template('index.html')
 
-@app.route('/sign-in', methods = ["GET"])
+@app.route('/sign-in', methods=['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    if (request.method == "GET"):
+        return render_template('signin.html')
+    elif (request.method == "POST"):
+        data = request.get_json()
+        user_name = data['Username']
+        passwrd = data["Password"]
 
-@app.route('/sign-in', methods = ['POST'])
-def login():
-    data = request.get_json()
-    user_name = data['Username']
-    passwrd = data["Password"]
-
-    user = User.query.filter_by(username = user_name).first()
-    if user:
-        access_token = create_access_token(identity=user.id)
-        return jsonify({"access_token": access_token}), 200
-    else:
-        return make_response("The account with this username dosen't exist or the password is wrong", 201)
-    
+        user = User.query.filter_by(username = user_name).first()
+        if user:
+            access_token = create_access_token(identity=user.id)
+            return jsonify({"access_token": access_token}), 200
+        else:
+            return make_response("The account with this username dosen't exist or the password is wrong", 201)
 
 @app.route("/dashboard")
 
-@app.route('/sign-up', methods = ["GET"])
+@app.route('/sign-up',methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    if (request.method == "GET"):
+        return render_template('signup.html')
+    elif (request.method == "POST"):
+        data = request.get_json()
+        user_name = data['Username']
+        passwrd = data["Password"]
 
+        users = User.query.filter_by(username = user_name).first()
+
+        if not users:
+            users = User(
+                id = str(uuid.uuid4()),
+                username = user_name,
+                password = passwrd )
+            db.session.add(users)
+            db.session.commit()
+        else:
+            return make_response("user already exists", 202)
 
 if __name__ == "__main__":
     app.run(debug=True)
