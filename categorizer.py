a419716cc76f215610b8b5a54d847bd45ce24a0f
Anish <ani.bellamkonda@gmail.com>
2024-12-29 10:55:51 -0600

    made sure it works

--- categorizer.py
+++ categorizer.py
@@ -2,8 +2,9 @@ from transformers import pipeline
 
 classifier = pipeline("zero-shot-classification")
 
-res = classifier{
-    "",
-    candidate_labels = ["STEM","Humanities","Arts","Health Sciences","Social Sciences"],
-}
+res = classifier(
+    "Mass Media Science & Engineering Fellowship through AAASThis program strengthens connections between scientists and journalists by placing undergraduate scientists, engineers, and mathematicians at media organizations (National Public Radio, Los Angeles Times, WIRED, etc.). Students sharpen their abilities to communicate complex scientific issues and enhance coverage of science-related issues in media.",
+    candidate_labels = ["STEM","Humanities","Arts","Health Sciences","Social Sciences"], 
+)
+print(res)
 
