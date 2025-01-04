5bc53c6fcac9b0145bef53406324c526412fc7dd
Anish <ani.bellamkonda@gmail.com>
2025-01-03 18:00:02 -0600

    changes

--- email_generation.py
+++ email_generation.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAS6fzwC7bqyKmcs24Om3jGPrgro9BUweQ")
+genai.configure(api_key="")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a outline for a cold email to a professor which is broad")
 print(response.text)
